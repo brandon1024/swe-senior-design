44b28dbb336e07e145e68f48fdbc614f6ff7d408
Brandon Richardson <brandon1024.br@gmail.com>
2018-08-18 22:40:56 +0000

    Continuous Integration Setup

--- src/test/java/com/unb/bucket/BucketListApplicationTests.java
+++ src/test/java/com/unb/bucket/BucketListApplicationTests.java
@@ -1,5 +1,6 @@
 package com.unb.bucket;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 class BucketListApplicationTests {
 
 	@Test void contextLoads() {
+		//Assertions.fail("failed");
 	}
-
 }
