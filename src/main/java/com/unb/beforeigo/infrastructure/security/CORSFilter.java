e5d835bfdfe2b670f4d921de680ed4a0789b1be5
Brandon <brandon1024.br@gmail.com>
2019-02-12 10:23:52 -0400

    CORS Filter Fix
    It was recently discovered that the CORS filter implemented in the
    backend only supported `simple` requests, and did not support preflight
    requests. This commit addresses this by modifying the
    Access-Control-Allow-Origin with a value that can be configured in the
    application.properties file.

--- src/main/java/com/unb/beforeigo/infrastructure/security/CORSFilter.java
+++ src/main/java/com/unb/beforeigo/infrastructure/security/CORSFilter.java
@@ -1,6 +1,7 @@
 package com.unb.beforeigo.infrastructure.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -14,13 +15,28 @@ import java.io.IOException;
 @Slf4j
 public class CORSFilter extends OncePerRequestFilter {
 
+    private static String allowedOrigin;
+
+    private static String maxAge;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
+        response.setHeader("Access-Control-Allow-Origin", CORSFilter.allowedOrigin);
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,PUT,OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Max-Age", "180");
+        response.setHeader("Access-Control-Max-Age", CORSFilter.maxAge);
+        response.setHeader("Vary", "Accept-Encoding, Origin");
         filterChain.doFilter(request, response);
+    }
+
+    @Value("${ui.origin:http://localhost:3000}")
+    public void setOrigin(final String origin) {
+        CORSFilter.allowedOrigin = origin;
+    }
+
+    @Value("${ui.cors.maxAge:86400}")
+    public void setMaxAge(final String maxAge) {
+        CORSFilter.maxAge = maxAge;
     }
 }
